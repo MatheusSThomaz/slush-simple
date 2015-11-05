549e64d9b63e0a864120e1a3b82478dc33281ad6
Matheus Thomaz <matheus.thomaz@ag2nurun.com.br>
2015-11-05 11:28:05 -0200

    Start project

---
+++ slushfile.js
@@ -0,0 +1,106 @@
+/*
+ * slush-simple
+ * https://github.com/MatheusSThomaz/slush-simple
+ *
+ * Copyright (c) 2015, Matheus Thomaz
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+var gulp        = require('gulp'),
+    install     = require('gulp-install'),
+    conflict    = require('gulp-conflict'),
+    template    = require('gulp-template'),
+    rename      = require('gulp-rename'),
+    _           = require('underscore.string'),
+    inquirer    = require('inquirer');
+
+function format(string) {
+  var username = string ? string.toLowerCase() : '';
+  return username.replace(/\s/g, '');
+}
+
+var defaults = (function () {
+  var homeDir = process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE,
+      workingDirName = process.cwd().split('/').pop().split('\\').pop(),
+      osUserName = homeDir && homeDir.split('/').pop() || 'root',
+      configFile = homeDir + '/.gitconfig',
+      user = {};
+  if (require('fs').existsSync(configFile)) {
+    user = require('iniparser').parseSync(configFile).user;
+  }
+  return {
+    appName: workingDirName,
+    userName: format(user.name) || osUserName,
+    authorEmail: user.email || ''
+  };
+})();
+
+console.log([
+    '',
+    '',
+    '               Slush Generator',
+    '            I hope you enjoy it :)',
+    '',
+    '     Matheus Thomaz | contato@msthomaz.com',
+    '',
+    ''
+  ].join('\n'));
+
+gulp.task('default', function (done) {
+  var prompts = [{
+      name: 'appName',
+      message: 'What is the name of your project?',
+      default: defaults.appName
+  }, {
+    name: 'appDescription',
+    message: 'What is the description?'
+  }, {
+    name: 'appVersion',
+    message: 'What is the version of your project?',
+    default: '0.1.0'
+  }, {
+    name: 'authorName',
+    message: 'What is the author name?',
+    default: 'Matheus Thomaz'
+  }, {
+    type: 'list',
+    name: 'selectPreprocessor',
+    message: 'What preprocessor you want to use?',
+    choices: [
+      { name: 'less', value: 'less' },
+    ],
+    default: 0
+  }, {
+    type: 'confirm',
+    name: 'moveon',
+    message: 'Continue?'
+  }];
+  //Ask
+  inquirer.prompt(prompts,
+    function (answers) {
+      if (!answers.moveon) {
+        return done();
+      }
+      answers.appNameSlug = _.slugify(answers.appName);
+      gulp.src(__dirname + '/templates/' + answers.selectPreprocessor + '/**')
+        .pipe(template(answers))
+        .pipe(rename(function(file) {
+          if (file.basename[0] === '_') {
+            file.basename = '.' + file.basename.slice(1);
+          }
+        }))
+        .pipe(rename(function(file) {
+          if (file.basename[0] === '-') {
+            file.basename = '_' + file.basename.slice(1);
+          }
+        }))
+        .pipe(conflict('./'))
+        .pipe(gulp.dest('./'))
+        .pipe(install())
+        .on('end', function () {
+          done();
+        });
+    });
+});
---
+++ templates/less/gulpfile.js
@@ -0,0 +1,75 @@
+var gulp      = require('gulp'),
+  less        = require('gulp-less'),
+  cssmin      = require('gulp-cssmin'),
+  browserSync = require('browser-sync'),
+  reload      = browserSync.reload,
+  uglify      = require('gulp-uglify'),
+  concat      = require('gulp-concat'),
+  changed     = require('gulp-changed'),
+  runSequence = require('run-sequence'),
+  plumber     = require('gulp-plumber'),
+  gutil       = require('gulp-util');
+
+var path = {
+  js: ['dev/assets/js/**/*.js', '!dev/assets/js/**/*.min.js'],
+  less: ['dev/assets/less/**/*.less'],
+  css: ['dev/assets/css/**/*.css', '!dev/assets/css/**/*.min.css'],
+  img: ['dev/assets/img/*'],
+  html: ['dev/**/*.html']
+};
+
+gulp.task('less', function () {
+  return gulp.src(path.less)
+    .pipe(changed('dev/assets/css/'))
+    .pipe(plumber({
+      errorHandler: function (err) {
+        console.log(err);
+        this.emit('end');
+      }
+    }))
+    .pipe(less())
+    .pipe(cssmin())
+    .pipe(gulp.dest('dev/assets/css/'))
+    .pipe(reload({stream:true}));
+});
+
+gulp.task('sync', function() {
+  browserSync({
+    server: {
+      baseDir: "./dev"
+    }
+  });
+});
+
+gulp.task('html', function () {
+  return gulp.src(path.html)
+    .pipe(reload({stream:true}));
+});
+
+gulp.task('watch', function () {
+  gulp.watch(path.less, ['less']);
+  gulp.watch(path.html, ['html']);
+});
+
+gulp.task('js:build', function () {
+  return gulp.src(path.js)
+    .pipe(uglify({outSourceMap: true}))
+    .on('error', gutil.log)
+    .pipe(gulp.dest('build/assets/js/'));
+});
+
+gulp.task('move:build', function () {
+  return gulp.src([ 
+    'dev/**',
+    '!dev/assets/less', 
+    '!dev/assets/less/**/*.less',
+    '!dev/assets/js',
+    '!dev/assets/js/**/*.js'
+  ])
+  .pipe(gulp.dest('build/'));
+});
+
+gulp.task('default', ['less', 'watch', 'sync']);
+gulp.task('build', function () {
+  runSequence('less', 'js:build', 'move:build');
+});
